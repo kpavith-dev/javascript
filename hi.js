2bbab3f1227de4a371cd243b3f767dbbe4723de3
pavipavi0308-maker <pavipavi0308@gmail.com>
2026-05-28 22:12:33 +0530

    first commit

---
+++ hi.js
@@ -0,0 +1,158 @@
+/*
+
+var a = 10
+var b = 20
+var c = a + b
+console.log(c)
+
+console.log("Hello JavaScript");
+
+
+
+let name = "John"
+console.log(name)
+
+const pi = 3.14
+console.log(pi)
+
+
+let age = 20
+console.log(age)
+
+age = 21
+
+console.log(age)
+
+
+var FruidName = "apple"
+console.log(FruidName)
+
+var Count = 20
+var price = 100
+var total = Count * price
+console.log(total)
+
+
+
+
+function a() {
+    console.log("Hello World")
+}
+a()
+
+
+
+
+
+var a = 10
+var b = 20
+
+function add() {
+    console.log(a + b)
+}
+add()
+
+
+
+var factor = "Tata Motors"
+function favourite() {
+
+    console.log("Favourite Actor : " + factor)
+
+}
+favourite()
+
+
+ 
+function area(a, b) {
+    console.log("Area of Rectangle : " + (a * b))
+}   
+area(5,10)
+
+
+
+function areaOfCircle(a) {
+    var r = a + 2 
+    console.log("Area of Circle : " + (3.14 * r * r))
+}   
+var a = 5
+areaOfCircle(a)
+
+
+function myname() {
+    return "My name is John"
+}       
+var a=myname()
+console.log(a)
+
+
+
+function add(a, b) {
+
+    return (a+b)
+}
+var sum = add(10,20)
+console.log(sum)
+
+
+
+var train = false
+
+if (train) {
+
+    console.log("Hello World")
+
+}
+else {
+    console.log("Hello JavaScript")
+}
+
+
+
+console.log(true && false)
+console.log(true || false)
+
+
+
+
+var colour = "red"
+
+if (colour == "green") {
+
+    console.log("you can go")
+}
+else {
+    console.log("waiting for the colour change")
+}
+    
+
+var sesion = "spring"
+if (sesion == "spring") {
+    console.log("The season is spring")
+}
+else if (sesion == "summer") {
+    console.log("The season is summer")
+}
+else if (sesion == "autumn") {
+    console.log("The season is autumn")
+}
+else if (sesion == "winter") {
+    console.log("The season is winter")
+}
+else {
+    console.log("invalid season")
+}
+
+
+var score = 85
+if (score <= 50) {
+    console.log("you need to imorove")
+}
+else if (score >= 51 && score <= 70) {
+    console.log("you are doing good")
+}
+else if (score >= 71) {
+    console.log("Excellent Performance")
+}
+    */
+
