51526a262141b91f558774cd84fcce75e8659bd1
pavipavi0308-maker <pavipavi0308@gmail.com>
2026-05-29 12:03:25 +0530

    for loop

--- hi.js
+++ hi.js
@@ -156,8 +156,18 @@ else if (score >= 71) {
 }
 
 
-*/
+
 for (var i = 1; i < 11; i++) {
     console.log("apple : " + i)
 }
+*/
+
+
+
+for (var i = 10; i >= 1; i--) {
+    console.log("apple : " + i * 2)
+}
+
+
+// javaScript DOM(document object model ) Manipulation
 
